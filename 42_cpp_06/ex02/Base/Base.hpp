686d1b7387b6dd584ec103507127c6bd0d44771b
Hatim Touil <htouil@c2r11p9.1337.ma>
2024-05-21 17:09:38 +0100

    starting ex02

---
+++ 42_cpp_06/ex02/Base/Base.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htouil <htouil@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 17:05:45 by htouil            #+#    #+#             */
+/*   Updated: 2024/05/21 17:07:38 by htouil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASE_HPP
+# define BASE_HPP
+
+# include <iostream>
+
+# define RESET       "\033[0m"
+# define RED         "\033[31m"
+# define GREEN       "\033[32m"
+# define YELLOW      "\033[33m"
+# define BLUE        "\033[34m"
+# define MAGENTA     "\033[35m"
+# define CYAN        "\033[36m"
+# define GREY        "\033[90m"
+# define WHITE       "\033[37m"
+# define GOLD        "\033[38;5;178m"
+# define ORANGE      "\033[38;5;202m"
+# define PINK        "\033[38;5;206m"
+# define PURPLE      "\033[38;5;141m"
+# define BROWN       "\033[38;5;94m"
+# define BEIGE       "\033[38;5;224m"
+# define OLIVE_GREEN "\033[38;5;58m"
+# define TEAL        "\033[38;5;30m"
+# define AQUA        "\033[38;5;51m"
+# define MAROON      "\033[38;5;52m"
+# define CRIMSON     "\033[38;5;160m"
+# define CORAL       "\033[38;5;203m"
+# define SALMON      "\033[38;5;209m"
+# define TURQUOISE   "\033[38;5;45m"
+# define INDIGO      "\033[38;5;54m"
+
+class Base
+{
+	
+};
+
+#endif
