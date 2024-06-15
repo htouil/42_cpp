51f80a7a05716f28f6e6fd437045a63bd8e959f7
Hatim Touil <htouil@c2r2p6.1337.ma>
2024-06-15 19:23:56 +0100

    looks good

--- 42_cpp_08/ex02/main.cpp
+++ 42_cpp_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: htouil <htouil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 01:25:09 by htouil            #+#    #+#             */
-/*   Updated: 2024/06/14 19:31:29 by htouil           ###   ########.fr       */
+/*   Updated: 2024/06/15 19:17:08 by htouil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,68 +16,61 @@
 int	main()
 {
 	{
-		std::cout << YELLOW << "------TEST 1------" << RESET << std::endl;
+		std::cout << GOLD << "------TEST 1------" << RESET << std::endl;
 
 		MutantStack<int>									mstack1;
 		MutantStack<int>::iterator							it;
 
+		mstack1.push(1);
+		mstack1.push(2);
+		mstack1.push(3);
+		mstack1.push(4);
 		mstack1.push(5);
-		mstack1.push(5);
-		mstack1.push(5);
-		mstack1.push(5);
-		mstack1.push(5);
-		std::cout << GOLD << "mstack1: " << RESET << std::endl;
+		std::cout << "mstack1: " ;
 		for (it = mstack1.begin(); it != mstack1.end(); it++)
-			std::cout << *it << " ";
+			std::cout << BLUE << *it << RESET << " ";
 		std::cout << std::endl;
 		it = mstack1.begin();
+		*it = 0;
+		std::cout << "first element of mstack1: " << GREEN << *it << std::endl;
 		*it = 1;
-		std::cout << "first element of mstack1: " << *it << std::endl;
-		*it = 5;
+
+		std::cout << std::endl << GOLD << "------TEST 2------" << RESET << std::endl;
 
 		MutantStack<int>					mstack2(mstack1);
 		MutantStack<int>::const_iterator	cit;
 
-		std::cout << GOLD << "mstack2: " << RESET << std::endl;
+		std::cout << "mstack2: " ;
 		for (cit = mstack2.cbegin(); cit != mstack2.cend(); cit++)
-			std::cout << *cit << " ";
+			std::cout << BLUE << *cit << RESET << " ";
 		std::cout << std::endl;
 		cit = mstack2.cbegin();
-		std::cout << "last element of mstack2: " << *cit << std::endl;
+		std::cout << "first element of mstack2: " << GREEN << *cit << std::endl;
 
+		std::cout << std::endl << GOLD << "------TEST 3------" << RESET << std::endl;
 		MutantStack<int>					mstack3;
 		MutantStack<int>::reverse_iterator	rit;
 
 		mstack3 = mstack1;
-		std::cout << GOLD << "mstack3: " << RESET << std::endl;
-		for (rit = mstack3.rend(); rit != mstack3.rbegin(); rit--)
-			std::cout << *rit << " ";
+		std::cout << "mstack3: ";
+		for (rit = mstack3.rbegin(); rit != mstack3.rend(); ++rit)
+			std::cout << BLUE << *rit << RESET << " ";
 		std::cout << std::endl;
-		rit = mstack3.rend();
+		rit = mstack3.rbegin();
+		*rit = 0;
+		std::cout << "last element of mstack3: " << GREEN << *rit << std::endl;
 		*rit = 1;
-		std::cout << "first element of mstack3: " << *rit << std::endl;
-		*rit = 5;
 
+		std::cout << std::endl << GOLD << "------TEST 4------" << RESET << std::endl;
 		MutantStack<int>							mstack4(mstack1);
 		MutantStack<int>::const_reverse_iterator	crit;
 
-		std::cout << GOLD << "mstack4: " << RESET << std::endl;
-		for (crit = mstack4.crend(); crit != mstack4.crbegin(); crit--)
-			std::cout << *crit << " ";
+		std::cout << "mstack4: ";
+		for (crit = mstack4.crbegin(); crit != mstack4.crend(); ++crit)
+			std::cout << BLUE << *crit << RESET << " ";
 		std::cout << std::endl;
-		crit = mstack4.crend();
-		std::cout << "last element of mstack4: " << *crit << std::endl;
+		crit = mstack4.crbegin();
+		std::cout << "last element of mstack4: " << GREEN << *crit << std::endl;
 	}
-
-	// {
-	// 	std::cout << std::endl;
-	// 	std::cout << "--------------------------------------------------------------------------" << std::endl;
-	// 	std::cout << std::endl;
-	// }
-
-	// {
-	// 	std::cout << YELLOW << "------TEST 2------" << RESET << std::endl;
-
-	// }
 	return (0);
 }
