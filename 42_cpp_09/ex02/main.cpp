0dfbabbd741acb10adb8da5d0b590213a44a8ad4
sorrow scavenger <hatim.touil0@gmail.com>
2024-07-23 21:58:18 +0100

    adding ex02

--- 42_cpp_09/ex02/main.cpp
+++ 42_cpp_09/ex02/main.cpp
@@ -5,27 +5,10 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: htouil <htouil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/15 19:24:18 by htouil            #+#    #+#             */
-/*   Updated: 2024/07/23 21:26:19 by htouil           ###   ########.fr       */
+/*   Created: 2024/07/23 21:22:07 by htouil            #+#    #+#             */
+/*   Updated: 2024/07/23 21:26:49 by htouil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "srcs/RPN.hpp"
+#include "srcs/PmergeMe.hpp"
 
-void	l()
-{
-	system("leaks RPN");
-}
-
-int	main(int ac, char **av)
-{
-	atexit(l);
-	if (ac == 2)
-		parse_calculate_expression(av[1]);
-	else
-	{
-		std::cerr << RED <<  "Error\n" << std::endl << "Wrong number of arguments!" << RESET << std::endl;
-		return (1);
-	}
-	return (0);
-}
--- 42_cpp_09/ex02/srcs/PmergeMe.cpp
+++ 42_cpp_09/ex02/srcs/PmergeMe.cpp
@@ -1,31 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: htouil <htouil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/15 19:24:18 by htouil            #+#    #+#             */
-/*   Updated: 2024/07/23 21:26:19 by htouil           ###   ########.fr       */
+/*   Created: 2024/07/23 21:23:18 by htouil            #+#    #+#             */
+/*   Updated: 2024/07/23 21:26:09 by htouil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "srcs/RPN.hpp"
+#include "PmergeMe.hpp"
 
-void	l()
-{
-	system("leaks RPN");
-}
-
-int	main(int ac, char **av)
-{
-	atexit(l);
-	if (ac == 2)
-		parse_calculate_expression(av[1]);
-	else
-	{
-		std::cerr << RED <<  "Error\n" << std::endl << "Wrong number of arguments!" << RESET << std::endl;
-		return (1);
-	}
-	return (0);
-}
